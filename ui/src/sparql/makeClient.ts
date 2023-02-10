3bb22fe9f957061b60c98e5da80db645bf3eeccc
Fumiya Kubota <fumiya.kubota@glucose.jp>
2023-02-10 13:45:15 +0900

    update

--- ui/src/sparql/makeClient.ts
+++ ui/src/sparql/makeClient.ts
@@ -1,5 +1,5 @@
 import ParsingClient from 'sparql-http-client/ParsingClient'
 
-const endpoint = process.env.NEXT_PUBLIC_SPARQL_ENDPOINT ?? 'http://kgrc4si.ml:7200/repositories/KGRC4SIv01'
+const endpoint = process.env.NEXT_PUBLIC_SPARQL_ENDPOINT ?? 'https://kgrc4si.ml/graph/repositories/KGRC4SIv01'
 
 export const makeClient = () => new ParsingClient({ endpointUrl: `${endpoint}?infer=false` })
